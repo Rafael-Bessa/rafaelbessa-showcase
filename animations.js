c95a11ba071e92dc42c02eeb291e7e386e7ade8b
Rafael Bessa <rafaelbessacivil@gmail.com>
2023-09-07 21:23:33 -0300

    Meu currículo

---
+++ animations.js
@@ -0,0 +1,6 @@
+// Este exemplo utiliza JavaScript para animar a entrada da seção de informações
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector(".container");
+    container.style.opacity = "1";
+    container.style.transform = "translateY(0)";
+});
